dd645195c7ef2641114f57df8881f9572fb12f71
ER1-53 <cremey.erwan@gmail.com>
2023-06-16 05:15:32 -0700

    4-puts.c

---
+++ hello_world/4-puts.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+ *main - Entry point
+ * 
+ *Retrun: Always 0 (Success)
+ */
+int main(void)
+{
+puts(char "Programming is like building a multilingual puzzle");
+return (0);
+}
