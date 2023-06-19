6fc3c6b60ee31f971f9f4219ced20ea4da6e3875
ER1-53 <cremey.erwan@gmail.com>
2023-06-19 13:25:57 -0700

    8-print_base164 git commit -m 8-print_base164

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+/**
+ *main - retry life
+ *
+ *Description: hexadecimal
+ *
+ *Return: big life
+ *
+ */
+
+int main(void)
+{
+	int letter;
+	int number;
+	for (number = 0; number < 10; number++)
+	{
+		putchar(number + '0');
+	}
+	for (letter = 'a'; letter <= 'f'; letter++)
+	{
+		putchar(letter);
+	}
+	putchar('\n');
+	return (0);
+}
