710565ec6d8272650ca5dffc1a0546d87d457382
ER1-53 <cremey.erwan@gmail.com>
2023-06-19 13:11:24 -0700

    7-print_tebahpla

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ *main - roots team
+ *
+ *Description: reverse alphabet
+ *
+ *Return: the world
+ */
+
+int main(void)
+{
+	int bop;
+
+	for (bop = 'z'; bop >= 'a'; bop--)
+	{
+		putchar(bop);
+	}
+	putchar('\n');
+	return (0);
+}
