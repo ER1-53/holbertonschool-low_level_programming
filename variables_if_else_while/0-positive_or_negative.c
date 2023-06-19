94bf8f121738c354c48bddc3c3c3c984621e3888
ER1-53 <cremey.erwan@gmail.com>
2023-06-19 04:31:01 -0700

    0-positive_or_negative

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+
+/**
+ *main - Entry point
+ *
+ *Descritpion: 'Random un nombre positif, negatif ou null'
+ *
+ *Return: Always o (Success)
+ */
+
+int main(void)
+{
+	int n;
+
+	srand(time(0));
+	n = rand() - RAND_MAX / 2;
+	if (n > 0)
+	{
+		printf("%d is positive", n);
+	} else if (n == 0)
+	{
+		printf("%d is zero", n);
+	} else
+	{
+		printf("%d is negative", n);
+	}
+	return (0);
+}
