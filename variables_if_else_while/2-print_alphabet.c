253cc8809651e70e3262eda45d38d598bfaf8805
ER1-53 <cremey.erwan@gmail.com>
2023-06-19 07:07:24 -0700

    2-print_alphabet

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ *main - Entry point
+ *
+ *Description: afficher l'alphabet
+ *
+ *Return: Success
+ */
+
+int main(void)
+{
+	int x;
+
+	for (x = 'a'; x <= 'y'; x++)
+	{
+		putchar(x);
+	}
+	putchar('\n');
+	return (0);
+}
