abc664930fcddd40e801ca0b6a95c14631684302
ER1-53 <cremey.erwan@gmail.com>
2023-06-20 07:22:10 -0700

    2-print_alphabet_x10.c

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ *print_alphabet_x10 - retry
+ *
+ *Description: print 10 alpha
+ *
+ *Return: if u want
+ */
+void print_alphabet_x10(void)
+{
+	int alpha;
+	int ten;
+
+	for (ten = 0; ten <= 10; ten++)
+	{
+		for (alpha = 'a'; alpha <= 'z'; alpha++)
+		{
+			_putchar(alpha);
+			_putchar('\n');
+		}
+	}
+}
