83b1c0c0d04763864a0886e63d19f5b70a14cdbd
ER1-53 <cremey.erwan@gmail.com>
2023-06-20 05:50:35 -0700

    0-putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,5 +1,4 @@
 #include "main.h"
-
 /**
  *main - function time
  *
@@ -10,6 +9,14 @@
 
 int main(void)
 {
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
 	_putchar('\n');
 	return (0);
 }
