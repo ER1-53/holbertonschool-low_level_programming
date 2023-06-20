9608456196ec1ee259bef537faac52392195f76f
ER1-53 <cremey.erwan@gmail.com>
2023-06-20 06:22:18 -0700

    1-alphabet

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+ *main - retry again again
+ *
+ *Description: print alpha in lowercase
+ *
+ *Return: Success
+ */
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
