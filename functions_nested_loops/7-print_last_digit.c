11eb45e8cf2fa17a724b72f1268111d9ec6c5771
ER1-53 <cremey.erwan@gmail.com>
2023-06-21 05:42:52 -0700

    7-print_last_digit.c

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ *print_last_digit - return
+ *@l: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int print_last_digit(int l)
+{
+	int lastdigit;
+	lastdigit = l % 10;
+	_putchar(lastdigit);
+	return (0);
+}
