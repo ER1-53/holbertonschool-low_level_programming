03c1247cbd597077cbe0d99ae9d5eb7b37451041
ER1-53 <cremey.erwan@gmail.com>
2023-06-21 13:46:13 -0700

    8-24

---
+++ functions_nested_loops/8-24_hours.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ *jack_bauer - return
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void jack_bauer(void)
+{
+	int c1, c2, c3, c4;
+
+	for (c4 = 0; c4 <= 2; c4++)
+	{
+		for (c3 = 0; c3 <= 3; c3++)
+		{
+			for (c2 = 0; c2 <= 5; c2++)
+			{
+				for (c1 = 0; c1 <= 9; c1++)
+				{
+					_putchar(c4 + '0');
+					_putchar(c3 + '0');
+					_putchar(':');
+					_putchar(c2 + '0');
+					_putchar(c1 + '0');
+					_putchar('\n');
+
+				}
+
+			}
+		}
+	}
+}
