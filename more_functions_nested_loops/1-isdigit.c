ea2ce71ef8b61bca1f2d4314d5bf95b624c4440f
ER1-53 <cremey.erwan@gmail.com>
2023-06-22 14:00:06 -0700

    1-isdigit.c

---
+++ more_functions_nested_loops/1-isdigit.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_function - return
+ *@c: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int _isdigit(int c)
+{
+	if (c >= 0 && c <= 9)
+	{
+		return (1);
+	}
+	else
+	{
+		return (0);
+	}
