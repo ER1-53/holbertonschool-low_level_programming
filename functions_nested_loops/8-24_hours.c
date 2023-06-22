32060386cc95c4c24af1e3512a5075edab32646a
ER1-53 <cremey.erwan@gmail.com>
2023-06-22 01:06:58 -0700

    8-24_hours.c

--- functions_nested_loops/8-24_hours.c
+++ functions_nested_loops/8-24_hours.c
@@ -13,21 +13,40 @@ void jack_bauer(void)
 
 	for (c4 = 0; c4 <= 2; c4++)
 	{
-		for (c3 = 0; c3 <= 3; c3++)
+		if (c4 == 1)
 		{
-			for (c2 = 0; c2 <= 5; c2++)
+			for (c3 = 0 ; c3 <= 9; c3++)
 			{
-				for (c1 = 0; c1 <= 9; c1++)
+				for (c2 = 0; c2 <= 5; c2++)
 				{
-					_putchar(c4 + '0');
-					_putchar(c3 + '0');
-					_putchar(':');
-					_putchar(c2 + '0');
-					_putchar(c1 + '0');
-					_putchar('\n');
-
+					for (c1 = 0; c1 <= 9; c1++)
+					{
+						_putchar(c4 + '0');
+						_putchar(c3 + '0');
+						_putchar(':');
+						_putchar(c2 + '0');
+						_putchar(c1 + '0');
+						_putchar('\n');
+					}
 				}
-
+			}
+		}
+		else
+		{
+			for (c3 = 0; c3 <= 3; c3++)
+			{
+				for (c2 = 0; c2 <= 5; c2++)
+				{
+					for (c1 = 0; c1 <= 9; c1++)
+					{
+						_putchar(c4 + '0');
+						_putchar(c3 + '0');
+						_putchar(':');
+						_putchar(c2 + '0');
+						_putchar(c1 + '0');
+						_putchar('\n');
+					}
+				}
 			}
 		}
 	}
---
+++ functions_nested_loops/9-times_table.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *_function - return
+ *@c: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+
+void times_table(void)
+{
+	int num0, num1, num2, num3, num4, num5, num6, num7, num8, num9;
+
+	for (num0 = 0; num0 <= 10; num0++)	{
+		_putchar('0');
+	}
+	for (num0 = 0; num0 <= 10; num0++)	{
+	for (num0 = 0; num0 <= 10; num0++)
+
+	{
+	for (num0 = 0; num0 <= 10; num0++){
+
+	for (num0 = 0; num0 <= 10; num0++)
+			{
+	for (num0 = 0; num0 <= 10; num0++)	{
+	for (num0 = 0; num0 <= 10; num0++)	{
+	for (num0 = 0; num0 <= 10; num0++)	{
+	for (num0 = 0; num0 <= 10; num0++)	{
+	for (num0 = 0; num0 <= 10; num0++)	{
