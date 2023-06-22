53290ea3de354f0133ea4da54a2241bc02b828bb
ER1-53 <cremey.erwan@gmail.com>
2023-06-22 15:04:13 -0700

    4-print_most_numbers

---
+++ more_functions_nested_loops/4-print_most_numbers.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *print_most_numbers - return
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void print_most_numbers(void)
+{
+	int num;
+
+	for (num = 0; num <= 9 ; num++)
+	{
+		if (num == 4 || num == 2)
+		{
+			continue;
+		}
+		else
+		{
+			_putchar(num + '0');
+		}
+	}
+}
