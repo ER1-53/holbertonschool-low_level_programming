33e6bf3c401014e3c3407c8dffe5d655bc9c2f8c
ER1-53 <cremey.erwan@gmail.com>
2023-06-23 00:45:37 -0700

    5-more_numbers

---
+++ more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,31 @@
+#include "main.h"
+
+/**
+ *more_numbers - return
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void more_numbers(void)
+{
+	int y;
+
+
+	for (y = 0; y <= 14; y++)
+	{
+		if (y > 9)
+		{
+			int u = y%10;
+			int v = y/10;
+			_putchar(v + '0');
+			_putchar(u + '0');
+		}
+		else
+		{
+			_putchar(y + '0');
+		}
+
+	}
+	_putchar('\n');
+}
