53f5e2220ebcf808f5e94e10c6777688da8f0391
ER1-53 <cremey.erwan@gmail.com>
2023-06-23 14:49:35 -0700

    11-print_to_98.C

---
+++ functions_nested_loops/11-print_to_98.C
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_to_98 - return
+ *@n: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void print_to_98(int n)
+{
+	int i;
+
+	for (i = 0; i < n; i++)
+	{
+		_putchar(98);
+	}
+}
