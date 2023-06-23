93281f7dfd00c0c2f5288a2821e36c184a7ae52d
ER1-53 <cremey.erwan@gmail.com>
2023-06-23 00:48:06 -0700

    5-more_numbers

--- more_functions_nested_loops/5-more_numbers.c
+++ more_functions_nested_loops/5-more_numbers.c
@@ -11,13 +11,13 @@ void more_numbers(void)
 {
 	int y;
 
-
 	for (y = 0; y <= 14; y++)
 	{
 		if (y > 9)
 		{
-			int u = y%10;
-			int v = y/10;
+			int u = y % 10;
+			int v = y / 10;
+
 			_putchar(v + '0');
 			_putchar(u + '0');
 		}
