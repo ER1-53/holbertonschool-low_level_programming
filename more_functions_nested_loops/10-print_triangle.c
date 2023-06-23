939fc871767ee6726ab484675c9668be16e0c156
ER1-53 <cremey.erwan@gmail.com>
2023-06-23 14:42:01 -0700

    10-print_triangle.c

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *print_triangle - return
+ *@size: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void print_triangle(int size)
+{
+	int i;
+
+	for (i = 0; i < size; i++)
+	{
+		_putchar('#');
+	}
+}
--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -16,24 +16,24 @@ int main(void)
 		if (turn % 3 == 0 && turn % 5 == 0)
 		{
 			printf("FizzBuzz");
-			if (turn != 100)
-			{
-			putchar(' ');
-			}
 		}
 		else if (turn % 5 == 0)
 		{
-			printf("Buzz ");
+			printf("Buzz");
 		}
 		else if (turn % 3 == 0)
 		{
-			printf("Fizz ");
+			printf("Fizz");
 		}
 		else
 		{
-			printf("%d ", turn);
+			printf("%d", turn);
 		}
-	}
-	putchar('\n');
+		if (turn <= 99)
+		{
+			printf(" ");
+		}
+       	}
+	printf("\n");
 	return (0);
 }
