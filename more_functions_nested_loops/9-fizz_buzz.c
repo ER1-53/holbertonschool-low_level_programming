7342c30fe4cadb53933664de6f0bed1a2a1778ac
ER1-53 <cremey.erwan@gmail.com>
2023-06-23 10:05:39 -0700

    fizz buzz

---
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+
+/**
+ *main - return
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int main(void)
+{
+	int turn;
+
+	for (turn = 1; turn <= 100; turn++)
+	{
+		if (turn % 3 == 0)
+		{
+			printf("Fizz ");
+		}
+		else if (turn % 5 == 0)
+		{
+			printf("Buzz ");
+		}
+		else if (turn % 5 == 0 && turn % 3 == 0)
+		{
+			printf("FizzBuzz ");
+		}
+		else
+		{
+			printf("%d ", turn);
+		}
+  	}
+	putchar('\n');
+}
