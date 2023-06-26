4fe8feafd398d27563454018acdce7f262449abb
ER1-53 <cremey.erwan@gmail.com>
2023-06-26 13:24:44 -0700

    3-puts

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_puts - display string
+ *@str: for string
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void _puts(char *str)
+{
+
+	while (*str != '\0')
+	{
+		_putchar(*str++);
+	}
+
+	_putchar('\n');
+}
