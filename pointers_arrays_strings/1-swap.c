20699b596c097437a258121d5bb8ce615952219a
ER1-53 <cremey.erwan@gmail.com>
2023-06-26 02:53:32 -0700

    1-swap

---
+++ pointers_arrays_strings/1-swap.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ *swap_int - return
+ *@a: is a value
+ *@b: is an other
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void swap_int(int *a, int *b)
+{
+	int invers;
+
+	invers = *a;
+	*a = *b;
+	*b = invers;
+}
