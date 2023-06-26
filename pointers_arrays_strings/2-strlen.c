543d2d800a03c6239b9dbaab41cecb6fb798b804
ER1-53 <cremey.erwan@gmail.com>
2023-06-26 12:40:01 -0700

    2-strlen

---
+++ pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ *_strlen - pour conmpterle nombre d'index
+ *@s: valeur de s pointeur
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int _strlen(char *s)
+{
+	int i;
+
+	while (s[i] != '\0')
+	{
+		i++;
+	}
+	return (i);
+}
