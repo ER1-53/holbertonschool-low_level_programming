88d8d447f466525b8fff1030c03494c2115a719d
ER1-53 <cremey.erwan@gmail.com>
2023-06-27 06:37:24 -0700

    fix final

--- pointers_arrays_strings/4-main.c
+++ pointers_arrays_strings/4-main.c
@@ -9,7 +9,7 @@ int main(void)
 {
 	char *str;
 
-	str = "I do not fear computers. I fear the lack of them - Isaac Asimov";
+	str = "Hello";
 	print_rev(str);
 	return (0);
 }
--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -16,7 +16,7 @@ void print_rev(char *s)
 	{
 		i++;
 	}
-	while (i >= 0)
+	while (i > 0)
 	{
 		i--;
 		_putchar(s[i]);
