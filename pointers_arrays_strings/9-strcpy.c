3a4dae01729abed207684cd414922c184968dbf9
ER1-53 <cremey.erwan@gmail.com>
2023-06-28 08:17:38 -0700

    9-strcpy

--- pointers_arrays_strings/9-strcpy.c
+++ pointers_arrays_strings/9-strcpy.c
@@ -12,11 +12,13 @@
 char *_strcpy(char *dest, char *src)
 {
 	int i;
-	for (i = 0; src[i] != '\0'; i++)
+
+	for (i = 0; src[i] != '\0' + 1; i++)
 	{
 		dest[i] = src[i];
 	}
-
+	dest[i + 1] = '\0';
+	
 	return (dest);
 
 
