7b479355a8b70ced348c4f225bb5f76647025c48
ER1-53 <cremey.erwan@gmail.com>
2023-06-28 07:07:03 -0700

    8-print_array

---
+++ pointers_arrays_strings/8-print_array.c
@@ -0,0 +1,27 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *print_array - return
+ *@a: is come back
+ *@n: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void print_array(int *a, int n)
+{
+	int i;
+	
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", a[i]);
+		if (i != n - 1)
+		{
+			printf(", ");
+		}
+	}
+
+
+	printf("\n");
+}
