411c55ef5133412c1f2b41aa3123b0db963ea16a
ER1-53 <cremey.erwan@gmail.com>
2023-06-29 13:47:46 -0700

    100-atoi test not final

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -1,11 +1,29 @@
 #include "main.h"
 
 /**
- *_function - return
- *@c: is come back
- *
- *Description: it want eat me
+ *_atoi - for change a string to number
+ *@s: is a caracter to be int
  *
  *Return: Success
  */
 int _atoi(char *s)
+{
+	int i = 0, a = 1, number;
+
+	while (s[i] != '\0')
+	{
+		if (s[i] == 45)
+		{
+			a = -1;
+			i++;
+		}
+
+		if (s[i] >= 48 || s[i] <= 57)
+		{
+			number = (number * 10) + (s[i] - 48);
+		}
+		i++;
+	}
+	number = number * a;
+	return (number);
+}
