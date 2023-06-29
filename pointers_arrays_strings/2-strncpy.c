8fe1589e3f15cd6ed4ab82b285abdb946d7c9882
ER1-53 <cremey.erwan@gmail.com>
2023-06-29 08:20:42 -0700

    2-strncpy fix default i=0

---
+++ pointers_arrays_strings/2-strncpy.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_strncpy - return
+ *@dest: is come back
+ *@src: next
+ *@n: again
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+char *_strncpy(char *dest, char *src, int n)
+{
+	int i = 0;
+
+	while (src[i] != '\0' && i < n)
+	{
+		dest[i] = src[i];
+		i++;
+	}
+	dest[i] = '\0';
+	return (dest);
+
+}
