0f63a9cb0a015cd21e6ae57da029be93ff5fa0a5
ER1-53 <cremey.erwan@gmail.com>
2023-06-29 14:08:59 -0700

    test 2

--- pointers_arrays_strings/100-atoi.c
+++ pointers_arrays_strings/100-atoi.c
@@ -8,22 +8,28 @@
  */
 int _atoi(char *s)
 {
-	int i = 0, a = 1, number;
+	int i = 0, a = 1, number = 0;
 
 	while (s[i] != '\0')
 	{
-		if (s[i] == 45)
+		if (s[i] == '-')
 		{
 			a = -1;
 			i++;
 		}
 
-		if (s[i] >= 48 || s[i] <= 57)
+		if (s[i] >= '0' || s[i] <= '9')
 		{
-			number = (number * 10) + (s[i] - 48);
+			number = (number * 10) + (s[i] - '0');
 		}
 		i++;
 	}
 	number = number * a;
+
+	if(number == 0)
+	{
+		return (0);
+	}
+
 	return (number);
 }
