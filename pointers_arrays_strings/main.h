5bfba333c0a834bcaec69df7f0ecce9bddb69089
ER1-53 <cremey.erwan@gmail.com>
2023-06-29 07:24:29 -0700

    1-strncat

--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -11,4 +11,5 @@ void print_array(int *a, int n);
 char *_strcpy(char *dest, char *src);
 int _atoi(char *s);
 char *_strcat(char *dest, char *src);
-
+char *_strncat(char *dest, char *src, int n);
+char *_strncpy(char *dest, char *src, int n);
