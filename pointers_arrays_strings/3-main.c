cf1a00d679f9119700c9af8896f450c055112ae2
ER1-53 <cremey.erwan@gmail.com>
2023-06-30 00:05:31 -0700

    3-strcmp

--- pointers_arrays_strings/3-main.c
+++ pointers_arrays_strings/3-main.c
@@ -8,7 +8,6 @@
  */
 int main(void)
 {
-<<<<<<< HEAD
 	char s1[] = "Hello";
 	char s2[] = "World!";
 
@@ -16,13 +15,4 @@ int main(void)
 	printf("%d\n", _strcmp(s2, s1));
 	printf("%d\n", _strcmp(s1, s1));
 	return (0);
-=======
-    char s1[] = "Hello";
-    char s2[] = "World!";
-
-    printf("%d\n", _strcmp(s1, s2));
-    printf("%d\n", _strcmp(s2, s1));
-    printf("%d\n", _strcmp(s1, s1));
-    return (0);
->>>>>>> a89f3ae576cf809143f2716af72543a5b8c197b5
 }
