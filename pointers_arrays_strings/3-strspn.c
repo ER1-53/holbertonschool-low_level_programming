3f2eb11ffdb2267eca086ced0fd9698ee3f7c9c8
ER1-53 <cremey.erwan@gmail.com>
2023-06-30 13:38:41 -0700

    2-strchr test

---
+++ pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,12 @@
+#include "main.h"
+
+/**
+ *_strspn - return
+ *@s: is come back
+ *@accept: next
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+unsigned int _strspn(char *s, char *accept)
--- pointers_arrays_strings/main.h
+++ pointers_arrays_strings/main.h
@@ -21,3 +21,4 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *_memcpy(char *dest, char *src, unsigned int n);
 char *_strchr(char *s, char c);
+unsigned int _strspn(char *s, char *accept);
