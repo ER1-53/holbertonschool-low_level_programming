4bf02198d039be06f22623dc9ae5bb6b483f083c
ER1-53 <cremey.erwan@gmail.com>
2023-07-03 08:02:28 -0700

    3-factorial.c

---
+++ recursion/3-factorial.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ *factorial - return
+ *@n: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int factorial(int n)
+{
+	if (n < 0)
+	{
+		return (-1);
+	}
+	if (n == 0)
+	{
+		return (1);
+	}
+	return (n * factorial(n - 1));
+}
--- recursion/main.h
+++ recursion/main.h
@@ -27,3 +27,4 @@ char *_strstr(char *haystack, char *needle);
 void _puts_recursion(char *s);
 void _print_rev_recursion(char *s);
 int _strlen_recursion(char *s);
+int factorial(int n);
