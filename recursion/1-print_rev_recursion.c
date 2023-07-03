102a94fc8705bd07b6e43c6b68518f8411ccfca9
ER1-53 <cremey.erwan@gmail.com>
2023-07-03 05:18:08 -0700

    1-print_rev_recursion

---
+++ recursion/1-print_rev_recursion.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ *_print_rev_recursion - La recursion rempli une pile qui incremente
+ *l'index de la chaine de caratère de la 1ere valeur à la dernière puis
+ *la redistribue à l'endroit si le putchar est avant l'appel de la fonction
+ *à l'envers si le putchar est après l'appel de la fonction.
+ *
+ *@s: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+void _print_rev_recursion(char *s)
+{
+	if (*s == '\0')
+	{
+		_putchar('\n');
+		return;
+	}
+
+	_print_rev_recursion(s + 1);
+	_putchar(*s);
+}
