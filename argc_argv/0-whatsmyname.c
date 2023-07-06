37bc19a1b660f12741c3e2e422e79c9c7fb704bd
ER1-53 <cremey.erwan@gmail.com>
2023-07-05 22:35:30 -0700

    0-whatsmyname.c

--- argc_argv/0-whatsmyname.c
+++ argc_argv/0-whatsmyname.c
@@ -1,14 +1,15 @@
 #include "main.h"
 #include <stdio.h>
 /**
- *_function - return
- *@c: is come back
+ *main - return
+ *@argc: number of argument
+ *@argv: string of argument
  *
  *Description: it want eat me
  *
  *Return: Success
  */
-int main(int argc __attribute__((unused)), char** argv)
+int main(int argc __attribute__((unused)), char **argv)
 {
 	printf("%s\n", argv[0]);
 	return (0);
