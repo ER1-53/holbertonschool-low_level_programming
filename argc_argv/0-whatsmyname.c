dc391f248c43a2026e87e52093bdd3cd016e7a1a
ER1-53 <cremey.erwan@gmail.com>
2023-07-05 22:32:33 -0700

    0-whatsmyname.c

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ *_function - return
+ *@c: is come back
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int main(int argc __attribute__((unused)), char** argv)
+{
+	printf("%s\n", argv[0]);
+	return (0);
+}
