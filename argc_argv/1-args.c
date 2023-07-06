b5194bd3f922f20884eda1c525ddc2e9942a2a89
ER1-53 <cremey.erwan@gmail.com>
2023-07-05 22:44:33 -0700

    1-args.c

---
+++ argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *main - return
+ *@argc: is come back
+ *@argv: the other
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int main(int argc, char **argv __attribute__((unused)))
+{
+	printf("%d\n", argc - 1);
+	return (0);
+}
