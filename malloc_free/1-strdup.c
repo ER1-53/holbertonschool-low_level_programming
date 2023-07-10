922fc9d67d4d1782e15463a72b3e5eb5faa8156a
ER1-53 <cremey.erwan@gmail.com>
2023-07-10 11:46:45 +0200

    1-strdup.c

--- malloc_free/1-strdup.c
+++ malloc_free/1-strdup.c
@@ -3,11 +3,42 @@
 #include <stdlib.h>
 
 /**
- *create_array - return
- *@size: is come back
- *@c: the other
+ *_strdup - Write a function that returns a pointer to a newly allocated
+ *space in memory, which contains a copy of the string given as a parameter.
+ *@str: is come back
  *
- *Description: it want eat me
+ *Description: il faut connaitre le nombre de caractère à copier
+ *on vérifie si str n'est pas vide sinon return NULL
+ *grace on nombre de caratère on connait l'espace qu'il faut allouer dans
+ *malloc pour copier le même mot.
  *
  *Return: Success
  */
+char *_strdup(char *str)
+{
+	int i;
+	char *pt_memory;
+
+	if (str == NULL)
+	{
+	return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+	}
+
+	pt_memory = malloc(sizeof(char) * (i + 1));
+
+	if (pt_memory == 0)
+	{
+		return (NULL);
+	}
+
+	for (i = 0; str[i] != '\0'; i++)
+	{
+		pt_memory[i] = str[i];
+	}
+	pt_memory[i] = '\0';
+	return (pt_memory);
+}
--- malloc_free/main.h
+++ malloc_free/main.h
@@ -13,6 +13,9 @@ void times_table(void);
 int add(int, int);
 void print_to_98(int n);
 char *create_array(unsigned int size, char c);
-
+char *_strdup(char *str);
+char *str_concat(char *s1, char *s2);
+int **alloc_grid(int width, int height);
+void free_grid(int **grid, int height);
 
 #endif /* MAIN_H */
