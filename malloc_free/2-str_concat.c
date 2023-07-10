c9701a29855acd38f0cfe8635c95362fa156ef31
ER1-53 <cremey.erwan@gmail.com>
2023-07-10 16:26:09 +0200

    FIX BUG

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -18,36 +18,35 @@ char *str_concat(char *s1, char *s2)
 	unsigned int i, j;
 	char *arr;
 
-	if (s1 == 0 && s2 == 0)
-	{
-		return (NULL);
-	}
-	for (j = 0 ; s2[j] != '\0' ; j++)
-	{
-	}
-	for (i = 0 ; s1[i] != '\0'; i++)
-	{
-	}
-	if (*s1 == 0 || *s2 == 0)
-	return (NULL);
-
+	if (s2 != NULL)
+		for (j = 0 ; s2[j] != '\0' ; j++)
+			j++;
+	if(s1 != NULL)
+		for (i = 0 ; s1[i] != '\0'; i++)
+			i++;
 	arr = malloc((i + j + 1) * sizeof(char));
-
 	if (arr == 0)
-	return (NULL);
+		return (NULL);
 	i = 0;
 
-	while (s1[i] != '\0')
+	if (s1 != NULL)
 	{
-		arr[i] = s1[i];
-		i++;
+		while (s1[i] != '\0')
+		{
+
+			arr[i] = s1[i];
+			i++;
+		}
 	}
 	j = 0;
 
-	while (s2[j] != '\0')
+	if (s2 != NULL)
 	{
-		arr[i + j] = s2[j];
-		j++;
+		while (s2[j] != '\0')
+		{
+			arr[i + j] = s2[j];
+			j++;
+		}
 	}
 	arr[i + j] = '\0';
 	return (arr);
