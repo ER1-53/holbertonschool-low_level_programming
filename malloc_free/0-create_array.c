e8b03cb7a121e808a562a90f5a4a5d78bd955805
ER1-53 <cremey.erwan@gmail.com>
2023-07-10 09:42:04 +0200

    init du proget malloc

---
+++ malloc_free/0-create_array.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ *create_array - return
+ *@size: is come back
+ *@c: the other
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+char *create_array(unsigned int size, char c)
+{
+	if (size == 0)
+	return NULL;
+
+	
+}
