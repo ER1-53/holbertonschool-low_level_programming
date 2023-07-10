3f7d2c84b155734e849742af5a95bb7eb5e5549f
ER1-53 <cremey.erwan@gmail.com>
2023-07-10 10:59:35 +0200

    explication du code 0-create array

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -3,11 +3,21 @@
 #include <stdlib.h>
 
 /**
- *create_array - return
+ *create_array - Write a function that creates an array of chars,
+ *and initializes it with a specific char.
  *@size: is come back
  *@c: the other
  *
- *Description: it want eat me
+ *Description: On cherche à allouer un espace 'size' pour le caractère 'c'
+ *on crée deux variables:
+ * 				unsigned int i : pour exploiter la totalité de l'espace alloué.
+ * 				char *arr : pointeur qui contient l'espace créé.
+ *Si 'size' == 0 la taille du tableeau n'est pas valide
+ *on alloue la mémoire pour le tableau arr = malloc(sizeof(char) * size)
+ *on vérifie si la mémoire est bien alloué su arr == null echec
+ *la boucle for attribue la valeur de c aux éléments du tableau.
+ *puis est returné le pointeur vers le bloc mémoire 'arr'
+ *
  *
  *Return: Success
  */
---
+++ malloc_free/1-strdup.c
@@ -0,0 +1,13 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ *create_array - return
+ *@size: is come back
+ *@c: the other
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
