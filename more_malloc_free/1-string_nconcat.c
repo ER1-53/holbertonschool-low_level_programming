5186ccbadd07088e7c8910777468adc021ecd40d
ER1-53 <cremey.erwan@gmail.com>
2023-07-11 16:04:11 +0200

    1-string_nconcat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -18,4 +18,41 @@
  *
  *Return: Success
  */
- char *string_nconcat(char *s1, char *s2, unsigned int n);
+ char *string_nconcat(char *s1, char *s2, unsigned int n)
+ {
+	int i, j;
+	char *arr;
+
+	if (s1 == NULL)
+		s1 = "";
+	if (s2 == NULL)
+		s2 = "";
+
+	for (i = 0 ; s1[i] != '\0'; i++)
+		i++;
+	for (j = 0 ; s2[j] != '\0' ; j++)
+		j++;
+
+	arr = malloc((i + j + 1) * sizeof(n));
+
+	if (arr == 0)
+		return (NULL);
+	i = 0;
+	j = 0;
+
+	while (s1[i] != '\0')
+	{
+		arr[i] = s1[i];
+		i++;
+	}
+
+	while (s2[j] != '\0')
+	{
+		arr[i] = s2[j];
+		i++;
+		j++;
+	}
+
+	arr[i] = '\0';
+	return (arr);
+ }
