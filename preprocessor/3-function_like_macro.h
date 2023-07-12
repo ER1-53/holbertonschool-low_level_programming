ab1d73009cb95f2f4a358e89e3e50722d9dad025
ER1-53 <cremey.erwan@gmail.com>
2023-07-12 17:44:13 +0200

    3-function_macro

--- preprocessor/3-function_like_macro.h
+++ preprocessor/3-function_like_macro.h
@@ -1,6 +1,6 @@
 #ifndef __3_FUNCTION_LIKE_MACRO_H__
 #define __3_FUNCTION_LIKE_MACRO_H__
 
-#define ABS(x)   ((x) < 0 ? (x * -1) : (X))
+#define ABS(x)   ((x) < 0 ? (x * -1) : (x))
 
 #endif /* 3_FUNCTION_LIKE_MACRO_H */
