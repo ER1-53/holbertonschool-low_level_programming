18f8819543dc1bc0176a35306241d81e7c17fdc5
ER1-53 <cremey.erwan@gmail.com>
2023-07-12 11:40:20 +0200

    1-string_cat

--- more_malloc_free/1-string_nconcat.c
+++ more_malloc_free/1-string_nconcat.c
@@ -3,49 +3,49 @@
 #include <stdlib.h>
 
 /**
- *_strdup - Write a function that returns a pointer to a newly allocated
+ *string_nconcat - Write a function that returns a pointer to a newly allocated
  *space in memory, which contains a copy of the string given as a parameter.
- *@str: is come back
+ *@s1: is come back
+ *@s2: is come back
+ *@n: is come back
  *
- *Description: il faut connaitre le nombre de caractère à copier
- *on vérifie si str n'est pas vide sinon return NULL
- *grace on nombre de caratère on connait l'espace qu'il faut allouer dans
- *malloc pour copier le même mot.
- *la 1ere boucle permet de comptabiliser le nombre de caractère
- *dans le mot
- *dans pt_memory on multiplit la valeur par 'i' + 1 (sans la valeur NULL)
- *Puis avec la 2nd boucle on copie la string de str[i] dans pt_memory[i].
+ *
+ *Description: that comming
  *
  *Return: Success
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int i, j;
+	unsigned int i, j, k;
 	char *arr;
 
 	if (s1 == NULL)
 		s1 = "";
 
 	for (i = 0; s1[i] != '\0'; i++)
-		;
+		{
+
+		}
 
 	for (j = 0; s2[j] != '\0'; j++)
-		;
+		{
+
+		}
 
 	arr = malloc((i + n + 1) * sizeof(char));
 
 	if (arr == NULL)
-		return NULL;
+		return (NULL);
 
-	for (unsigned int k = 0; k < i; k++)
+	for (k = 0; k < i; k++)
 		arr[k] = s1[k];
 
-	for (unsigned int k = 0; k < n && s2[k] != '\0'; k++)
+	for (k = 0; k < n && s2[k] != '\0'; k++)
 		arr[i + k] = s2[k];
 
 	arr[i + n] = '\0';
 
-	return arr;
+	return (arr);
 }
 
 
