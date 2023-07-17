f4d9c526f1009837561db7c5fed28b5ca55d2a5b
ER1-53 <cremey.erwan@gmail.com>
2023-07-17 10:53:48 +0200

    modif betty

--- structures_typedef/dog.h
+++ structures_typedef/dog.h
@@ -1,6 +1,15 @@
 #ifndef DOG_H
 #define DOG_H
 
+/**
+ * struct dog - struct to main.c
+ * @name: dog name
+ * @age: dog age
+ * @owner: dog owner
+ *
+ * Description: struct of the listing of the dog
+ */
+
 struct dog
 {
 	char *name;
