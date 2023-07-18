34516cb017b2a24ea8410e6b4e1711dc0ab7ce7f
ER1-53 <cremey.erwan@gmail.com>
2023-07-18 15:53:31 +0200

    task 2 betty fix and -1 for NULL

--- function_pointers/2-int_index.c
+++ function_pointers/2-int_index.c
@@ -19,11 +19,8 @@ int int_index(int *array, int size, int (*cmp)(int))
 {
 	int i;
 
-	if (size <= 0)
+	if (size <= 0 || array == NULL || cmp == NULL)
 		return (-1);
-	if (cmp == NULL)
-		return (0);
-
 
 	for (i = 0; i < size; i++)
 	{
