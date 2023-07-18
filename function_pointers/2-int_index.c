b4d36b3d056d6f61965bdbc32868883fba59c889
ER1-53 <cremey.erwan@gmail.com>
2023-07-18 15:49:58 +0200

    task 2

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,43 @@
+#include "main.h"
+#include "function_pointers.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+
+/**
+ *int_index - return
+ *@array: information of the array
+ *@size: the number of elements in the array
+ *@cmp: pointer to the function to be used to compare values
+ *
+ *
+ *Description: it want eat me
+ *
+ *Return: Success
+ */
+int int_index(int *array, int size, int (*cmp)(int))
+{
+	int i;
+
+	if (size <= 0)
+		return (-1);
+	if (cmp == NULL)
+		return (0);
+
+
+	for (i = 0; i < size; i++)
+	{
+		if (cmp(array[i]) == 0)
+		{
+			continue;
+		}
+		else
+		{
+			return (i);
+		}
+	}
+
+
+	return (-1);
+
+}
