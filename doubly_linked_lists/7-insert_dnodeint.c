66de09c94bf3944e3fdd64f214e9f339bdbb4e65
ER1-53 <cremey.erwan@gmail.com>
2023-08-03 21:13:22 +0200

    task 7 & 8

---
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -0,0 +1,58 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "lists.h"
+
+/**
+ * insert_dnodeint_at_index - prints then returns the length of element
+ *
+ * @h: pointer to the list
+ * @idx: pointer to the list
+ * @n: pointer to the list
+ *
+ * Return: temp.
+ */
+
+dlistint_t *insert_dnodeint_at_index(dlistint_t **h, unsigned int idx, int n)
+{
+	unsigned int i = 0;
+	dlistint_t *cp_head = NULL, *new_node = NULL;
+
+	if (h == NULL)
+		return (NULL);
+
+	new_node = (dlistint_t *)malloc(sizeof(dlistint_t));
+
+	if (new_node == NULL)
+		return (NULL);
+
+	new_node->n = n;
+	new_node->prev = NULL;
+	new_node->next = NULL;
+
+	if (idx == 0)
+	{
+		if (*h != NULL)
+			(*h)->prev = new_node;
+
+		new_node->next = *h;
+		*h = new_node;
+		return (new_node);
+	}
+	cp_head = *h;
+	while (i < idx - 1 && cp_head != NULL)
+	{ cp_head = cp_head->next;
+	i++; }
+
+	if (cp_head == NULL)
+	{ free(new_node);
+		return (NULL); }
+
+	new_node->next = cp_head->next;
+	if (cp_head->next != NULL)
+		cp_head->next->prev = new_node;
+
+	cp_head->next = new_node;
+	new_node->prev = cp_head;
+	return (new_node);
+}
--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -4,23 +4,48 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - prints then returns the length of element
+ * delete_dnodeint_at_index - prints then returns the length of element
  *
  * @head: pointer to the list
+ * @index: pointer to the list
  *
  * Return: temp.
  */
 
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
-
-
-
-if (*head == NULL)
-	return (-1);
-
-while (index != 0)
 {
-	head = head->next;
-		index--;
+	dlistint_t *pop_node;
+	unsigned int i = 0;
+
+	if (*head == NULL)
+		return (-1);
+
+	pop_node = *head;
+
+	while (i < index && pop_node != NULL)
+	{
+		pop_node = pop_node->next;
+		i++;
+	}
+
+	if (pop_node == NULL)
+		return (-1);
+
+	if (pop_node->prev != NULL)
+		pop_node->prev->next = pop_node->next;
+	else
+		*head = pop_node->next;
+
+	if (pop_node->next != NULL)
+		pop_node->next->prev = pop_node->prev;
+
+	free(pop_node);
+	return (1);
 }
+
+
+
+
+
+
 
