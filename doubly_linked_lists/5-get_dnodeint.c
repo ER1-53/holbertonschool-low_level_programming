acdc20acf57200322ef42024dea177ac731703b4
ER1-53 <cremey.erwan@gmail.com>
2023-08-03 12:34:51 +0200

    modif exo 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -17,11 +17,15 @@ dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 	if (head == NULL)
 		return (NULL);
 
+
 	while (index != 0)
 	{
+		if (head->next == NULL)
+			return (NULL);
+			
 		head = head->next;
 		index--;
 	}
-	
-	return(head);
+
+	return (head);
 }
