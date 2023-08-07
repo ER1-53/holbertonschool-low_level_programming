6cbb88f447a58ec765a84c1879d50890ceb5dacb
ER1-53 <cremey.erwan@gmail.com>
2023-08-07 17:21:18 +0200

    modif task 0

--- file_io/0-read_textfile.c
+++ file_io/0-read_textfile.c
@@ -25,18 +25,18 @@ ssize_t read_textfile(const char *filename, size_t letters)
 
 	op = open(filename, O_RDONLY);
 	if (op == -1)
-		return (2);
+		return (0);
 
 	buf = (char *)malloc(letters);
 	if (!buf)
 	{
 		close(op);
-		return (2);
+		return (0);
 	}
 
 	rd = read(op, buf, letters);
 	if (rd == -1)
-		return (2);
+		return (0);
 
 	write(1, buf, rd);
 
