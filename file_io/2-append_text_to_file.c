16a5b9986020445746a420cee24eeeb34054f11e
ER1-53 <cremey.erwan@gmail.com>
2023-08-08 14:05:20 +0200

    fix bug if text_content == NULL

--- file_io/2-append_text_to_file.c
+++ file_io/2-append_text_to_file.c
@@ -8,25 +8,26 @@
 #include <sys/stat.h>
 
 /**
- * create_file - print a list
+ * append_text_to_file - print a list
  *
  * @filename: pointer to the list
  * @text_content: pointer to the list
  *
- * Return: rd.
+ * Return: op.
  */
 int append_text_to_file(const char *filename, char *text_content)
 {
 	int op, text_len, bytes;
 
-	if (!filename)
+	if (!filename || !text_content)
 		return (-1);
 
 	text_len = strlen(text_content);
 
+
 	op = open(filename, O_WRONLY | O_APPEND);
 	bytes = write(op, text_content, text_len);
-	
+
 	if (op == -1 || bytes == -1)
 		return (-1);
 
