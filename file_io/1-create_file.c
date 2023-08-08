f28a6c284a44f9fa94389f0495252b9d744f45ce
ER1-53 <cremey.erwan@gmail.com>
2023-08-08 12:01:24 +0200

    exo 1 permission activ

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -17,13 +17,13 @@
  */
 int create_file(const char *filename, char *text_content)
 {
-	int op, text_len, bytes;
+	int op, text_len, bytes, mode;
 
 
 	if (!filename)
 		return (-1);
-
-	op = open(filename, O_RDWR | O_CREAT | O_TRUNC);
+	mode = S_IRUSR | S_IWUSR;
+	op = open(filename, O_RDWR | O_CREAT | O_TRUNC, mode);
 	if (op == -1)
 		return (-1);
 
