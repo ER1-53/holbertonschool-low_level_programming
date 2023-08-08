24bb8216a21103f1c254d8f5e58925588c8e4ab9
ER1-53 <cremey.erwan@gmail.com>
2023-08-08 19:49:28 +0200

    trie

--- file_io/3-cp.c
+++ file_io/3-cp.c
@@ -33,7 +33,7 @@ int main(int argc, char **argv)
 
 	if (op_file1 == -1 || rd == -1)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %S\n", argv[1]);
+		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
 		close(op_file1);
 		exit(98);
 	}
