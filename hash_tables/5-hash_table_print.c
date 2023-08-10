3415bc7d2769adc09092078116c80f4628e926b3
ER1-53 <cremey.erwan@gmail.com>
2023-08-10 14:58:31 +0200

    task 5

---
+++ hash_tables/5-hash_table_print.c
@@ -0,0 +1,43 @@
+#include <stdlib.h>
+#include <string.h>
+#include <stdio.h>
+#include "hash_tables.h"
+
+/**
+ * hash_table_get - check the code for
+ *
+ * @ht: taille du
+ * @key: taille du
+ *
+ *
+ * Return: Always EXIT_SUCCESS.
+ */
+
+void hash_table_print(const hash_table_t *ht)
+{
+	unsigned long int i;
+	hash_node_t *element;
+
+	if (ht == NULL)
+	{
+		return;
+	}
+
+	printf("{");
+	for (i = 0; i < ht->size; i++)
+	{
+		element = ht->array[i];
+		while (element != NULL)
+		{
+			printf("'%s': '%s'", element->key, element->value);
+			if (element->next != NULL)
+			{
+				printf(", ");
+			}
+			element = element->next;
+		}
+	}
+	printf("}\n");
+}
+
+
