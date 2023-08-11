9c561ceb1fec4417bea89b79530f25dbbd4bf1a6
ER1-53 <cremey.erwan@gmail.com>
2023-08-11 15:59:40 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -18,7 +18,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	int hash_key;
 	hash_node_t *new_node;
 
-	if (ht == NULL || key == NULL || value == NULL)
+	if (*ht == NULL || *key == NULL || *value == NULL)
 		return (0);
 
 	new_node = (hash_node_t *)malloc(sizeof(hash_node_t));
