070c690526bbc8731fa415144deac29f4e8406bb
ER1-53 <135108281+ER1-53@users.noreply.github.com>
2023-09-09 09:10:49 +0200

    Update 3-factorial.c with description

--- recursion/3-factorial.c
+++ recursion/3-factorial.c
@@ -4,7 +4,23 @@
  *factorial - return
  *@n: is come back
  *
- *Description: it want eat me
+ *Description: on cherche à factoriser 'n' c'est à dire
+ *multiplier n et chque n-1 ensemble
+ *Par exemple 5! 5*4*3*2*1 sans arriver sur n-1 = 0
+ *ni n < 0 sinon bug du prog.
+ *
+ *on return la formule (n * fact(n-1)
+ *soit si n = 5
+ *5 * (5-1)
+ *5*4 * (4-1)
+ *5*4*3 * (3-1)
+ *5*4*3*2 * (2-1)
+ *5*4*3*2*1 * (1-1) = 0
+ *
+ *on return 1
+ *1*5*4*3*2*1 = 120
+ *à la fin de la fonction la pile d'opération s'effectu
+ *et affiche le produit.
  *
  *Return: Success
  */
