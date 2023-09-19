24bca21b17ebf450bcdab91dfa9a33cf5b8f591d
ER1-53 <cremey.erwan@gmail.com>
2023-09-19 20:39:01 +0200

    betty update with description

--- search_algorithms/0-linear.c
+++ search_algorithms/0-linear.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+/**
+ * linear_search - Recherche linéaire d'une valeur dans un tableau.
+ * @array: Le tableau d'entiers.
+ * @size: La taille du tableau.
+ * @value: La valeur à rechercher.
+ *
+ * Return: L'index de la première occurrence de la valeur, ou -1
+ * si non trouvée.
+ */
+
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
