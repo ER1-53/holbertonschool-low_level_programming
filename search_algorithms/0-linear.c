0b3006a07f55767b9aa5b75de2e677b090aab4a6
ER1-53 <cremey.erwan@gmail.com>
2023-09-19 19:42:20 +0200

    creat funct lineaar.c & README & search algos.h

---
+++ search_algorithms/0-linear.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int linear_search(int *array, size_t size, int value)
+{
+	size_t i;
+
+	if (array == NULL || !value)
+		return (-1);
+
+	for (i = 0; i <= size - 1; i++)
+	{
+		printf("value checked array[%ld] = [%d]\n", i, array[i]);
+		if (array[i] == value)
+		{
+			return (i);
+		}
+	}
+
+	return (-1);
+}
+
